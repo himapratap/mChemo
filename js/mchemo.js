07eca58a21bb8a528cd44dcc9b7ae83c3d660009
himapratap <himapratap@gmail.com>
2017-05-22 22:37:27 -0400

    pluralised treatment section

--- js/mchemo.js
+++ js/mchemo.js
@@ -1,14 +1,12 @@
 // Toggle right arrows and down arrows
 $('.side-nav li a').click(function() {
     console.log('mainLevel');
-    $('.side-nav li.open').addClass('active');
-
+ 
     $(this).find('.mainLevel').toggleClass('fa-caret-down fa-caret-right');
 });
 
 $('.side-nav li ul li a').click(function() {
     console.log('secondLevel');
-    $('.side-nav li ul li').addClass('active');
 
     $(this).find('.secondLevel').toggleClass('fa-plus-square fa-minus-square');
  });
@@ -17,5 +15,4 @@ $('.side-nav li ul li a').click(function() {
 $('.side-nav li ul li ul li a').click(function() {
     console.log('thirdLevel');
     $(this).find('.thirdLevel').toggleClass('fa-plus-square fa-minus-square');
-$('.side-nav li ul li ul li').addClass('active');
-});
+ });
